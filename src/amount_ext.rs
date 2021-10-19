f305b99ddd9f9910807489dac2bc2cebb2bc9dc9
keblek <68111648+keblek@users.noreply.github.com>
2021-10-19 09:40:25 -0500

    Add 'k' and 'm' in cli options parser
    Lets a user type 15ksat instead 15000sat. This is a minor feature that I find is missing from most cli's that handle sats.
    
    I'm trying to get the tests to run on my machine. They should all pass as its a very small change.

--- src/amount_ext.rs
+++ src/amount_ext.rs
@@ -9,14 +9,11 @@ pub trait FromCliStr: Sized {
 
 impl FromCliStr for Amount {
     fn from_cli_str(string: &str) -> anyhow::Result<Self> {
+        let string = string.replace("k", "000").replace("m", "000000").replace(" ", "");
         match string.rfind(char::is_numeric) {
             Some(i) => {
                 let denom = Denomination::from_str(&string[(i + 1)..])?;
-                let value: String = string[..=i]
-                    .chars()
-                    .filter(|c| !c.is_whitespace())
-                    .collect();
-
+                let value = String::from_str(&string[..=i])?;
                 Ok(Amount::from_str_in(&value, denom)?)
             }
             None => Err(anyhow!("{} is not a Bitcoin amount")),
@@ -28,10 +25,31 @@ impl FromCliStr for Amount {
 mod test {
     use super::*;
     #[test]
-    fn test_parse_value() {
+    fn test_parse_value_btc() {
         assert_eq!(
             Amount::from_cli_str("0.01BTC").unwrap(),
             Amount::from_sat(1_000_000)
+        );
+    }
+    #[test]
+    fn test_parse_value_sat() {
+        assert_eq!(
+            Amount::from_cli_str("100sat").unwrap(),
+            Amount::from_sat(100)
+        );
+    }
+    #[test]
+    fn test_parse_k_suffix() {
+        assert_eq!(
+            Amount::from_cli_str("15ksat").unwrap(),
+            Amount::from_sat(15_000)
+        );
+    }
+     #[test]
+    fn test_parse_m_suffix() {
+        assert_eq!(
+            Amount::from_cli_str("10msat").unwrap(),
+            Amount::from_sat(10_000_000)
         );
     }
 }
