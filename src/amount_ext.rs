6508c3597e59494b72bcaed782e7997a2d9ceb7c
keblek <68111648+keblek@users.noreply.github.com>
2022-02-24 10:23:53 -0500

    Added additional parsing logic
    Avoids checking for k or m if there is a denomination

--- src/amount_ext.rs
+++ src/amount_ext.rs
@@ -9,7 +9,11 @@ pub trait FromCliStr: Sized {
 
 impl FromCliStr for Amount {
     fn from_cli_str(string: &str) -> anyhow::Result<Self> {
-        let string = string.replace("k", "000").replace("m", "000000").replace(" ", "");
+        // check for suffix
+        if let Some(value) = parse_suffix(string) {
+            return value;
+        }
+        // parse normal input
         match string.rfind(char::is_numeric) {
             Some(i) => {
                 let denom = Denomination::from_str(&string[(i + 1)..])?;
@@ -21,9 +25,52 @@ impl FromCliStr for Amount {
     }
 }
 
+fn parse_suffix(string: &str) -> Option<Result<Amount, anyhow::Error>> {
+    let final_value: f32;
+    if void_denominations(string) {
+        match string.rfind(char::is_numeric) {
+            Some(i) => {
+                let user_value = &string[..=i].parse::<f32>().unwrap();
+                if check_k(string) {
+                    final_value = 1_000 as f32;
+                } else if check_m(string) {
+                    final_value = 1_000_000 as f32;
+                } else {
+                    return Some(Err(anyhow!(
+                        "'{}' is missing a denomination or a suffix, like k or m",
+                        string
+                    )));
+                }
+                return Some(Ok(Amount::from_sat((user_value * final_value) as u64)));
+            }
+            None => return Some(Err(anyhow!("'{}' is not a Bitcoin amount", string))),
+        };
+    }
+    None
+}
+
+fn check_m(string: &str) -> bool {
+    string.to_lowercase().contains("m")
+}
+
+fn check_k(string: &str) -> bool {
+    string.to_lowercase().contains("k")
+}
+
+fn void_denominations(string: &str) -> bool {
+    !(string.to_lowercase().ends_with("sat") | string.to_lowercase().ends_with("btc"))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    #[should_panic(
+        expected = "called `Result::unwrap()` on an `Err` value: unknown denomination: kBTC"
+    )]
+    fn test_parse_failing_btc() {
+        Amount::from_cli_str("1kBTC").unwrap();
+    }
     #[test]
     fn test_parse_value_btc() {
         assert_eq!(
@@ -41,15 +88,16 @@ mod test {
     #[test]
     fn test_parse_k_suffix() {
         assert_eq!(
-            Amount::from_cli_str("15ksat").unwrap(),
+            Amount::from_cli_str("15k").unwrap(),
             Amount::from_sat(15_000)
         );
     }
-     #[test]
+    #[test]
     fn test_parse_m_suffix() {
         assert_eq!(
-            Amount::from_cli_str("10msat").unwrap(),
-            Amount::from_sat(10_000_000)
+            Amount::from_cli_str("1.5m").unwrap(),
+            Amount::from_sat(1_500_000)
         );
     }
 }
+
